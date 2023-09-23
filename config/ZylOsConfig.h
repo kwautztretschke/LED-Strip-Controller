5613f20585db8ce8938e5a620ca847ae47ceb8b1
Victoria Stöffler <victoria.stoeffler@tum.de>
2023-09-23 16:02:32 +0200

    added config for Billi

--- config/ZylOsConfig.h
+++ config/ZylOsConfig.h
@@ -1,9 +1,10 @@
 #ifndef __ZYLOSCONFIG_H__ 
 #define __ZYLOSCONFIG_H__
 
-#define Z_DEVICE_NAME "tollerESP"
+#define Z_DEVICE_NAME "Billi"
 #define Z_WS2812B
-#define FB_SIZE 30
+// one window is 52 LEDs (13x4)
+#define FB_SIZE 208
 #define Z_DEFAULT_PROGRAM "solidColor"
 
 #endif // __ZYLOSCONFIG_H__
