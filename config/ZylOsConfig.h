0e0a276eab5ccd35d49b9ab621841e52c3567292
Victoria Stöffler <victoria.stoeffler@tum.de>
2023-10-16 03:41:21 +0200

    added new window

--- config/ZylOsConfig.h
+++ config/ZylOsConfig.h
@@ -3,8 +3,8 @@
 
 #define Z_DEVICE_NAME "Billi"
 #define Z_WS2812B
-// one window is 52 LEDs (13x4)
-#define FB_SIZE 208
+// one window is 52 LEDs (13x4), 4 Windows means 208, plus the smaller one is (9x7) = 63
+#define FB_SIZE 271
 #define Z_DEFAULT_PROGRAM "solidColor"
 
 #endif // __ZYLOSCONFIG_H__
